e703d0d48ddf5ac75f2c31d7947a31f5cc9e0558
GITGUSH <gustavosfiocco@gmail.com>
2026-09-10 22:21:48 -0300

    Commit do exercício 4 da lista 2

---
+++ ListaExercicios2/exercicio4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXERCÍCIO 4</title>
+</head>
+<body>
+    <h1>Exercício 4 - Lista 2</h1>
+    <form method="post" action="resposta4.php">
+        <label for="valor1">Informe o número do mês:</label>
+        <br>
+        <input type="number" id="valor1" name="valor1"><br>
+</body>
+</html>
